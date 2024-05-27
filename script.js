b106d9c5d18eb18fbc3500f3f81c1a020606896a
basquetlover <85557755+basquetlover@users.noreply.github.com>
2024-05-27 19:20:23 +0200

    intento añadir 404

--- script.js
+++
@@ -1,6 +0,0 @@
-window.addEventListener('load', function(){
-  document.getElementById("loader").classList.toggle("loader2")
-  setTimeout(function() {
-    document.getElementById("btn-descarga-noti").classList.remove("spawn");
-  }, 5000);
-});
